b8fe5bcf18ea26096550d817e2c067d6724eab1a
AndreLG84 <andre.long@rocketmail.com>
2022-07-18 13:36:34 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+//titre acceuil
+gsap.from("",{duration:2, y:-800});
+// array.from() permet de crée un tableau des element recueprer
+const forhover = Array.from(document.getElementsByClassName('forhover'))
+const all = forhover.length
+
+forhover.forEach(element => {
+    //donc en appellant forhover ici on appelle tout le tableau
+    element.addEventListener('mouseenter', () => {
+        //ici element est l'équivalent du element[i] d'un For, en gros il cible celui qui sra clique/hover
+        forhover.forEach(element => {
+            element.classList.remove('test')
+        })
+        element.classList.add('test')
+    })
+
+    element.addEventListener('mouseleave', () => {
+        forhover.forEach(element => {
+            element.classList.add('test')
+        });
+    })
+})
+
+const categorie = document.getElementById('categorie')
+const btnCategory = document.getElementById('btnCategory')
+
+btnCategory.addEventListener('mouseenter', function(){
+    categorie.classList.add('block')
+})
+categorie.addEventListener('mouseleave', function(){
+    categorie.classList.remove('block')
+})
