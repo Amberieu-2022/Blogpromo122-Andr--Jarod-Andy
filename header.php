ab9cee77436023502cb08816f0c3c4d3ae1dd21b
AndreLG84 <andre.long@rocketmail.com>
2022-07-18 14:42:46 +0200

    ul id=""

--- header.php
+++ header.php
@@ -11,9 +11,9 @@
     <header>
         <nav id="head"> 
             <div class="logo">
-                <img src="<?php echo get_template_directory_uri(); ?>/inc/img/IMG_0097" style="width: 130px; height: 130px" alt="index.php">
+                <img src="<?php echo get_template_directory_uri(); ?>/inc/img/IMG_0097" style="width: 130px; height: 130px" href="index.php">
             </div>
-            <ul>
+            <ul id="menu">
                 <li><a class="forhover test" href="index.php">Les + populaires</a></li>
                 <li><a class="forhover test"  href="<?php echo get_category_link(8); ?>"><?php echo get_cat_name(8); ?></a></li>
                 <li><a class="forhover test"  href="<?php echo get_category_link(1); ?>"><?php echo get_cat_name(1); ?></a></li>
@@ -31,7 +31,7 @@
         </div>
         <nav id="categorie">
             <h1>Choisir une catégorie</h1>
-            <ul>
+            <ul id="left-menu">
                 <li><a class="forhover test"  href="<?php echo get_category_link(8); ?>"><?php echo get_cat_name(8); ?></a></li>
                 <li><a class="forhover test"  href="<?php echo get_category_link(1); ?>"><?php echo get_cat_name(1); ?></a></li>
                 <li><a class="forhover test" href="<?php echo get_category_link(7); ?>"><?php echo get_cat_name(7); ?></a></li>
